1a986cf10de31d7d2388e152dec0d37f367f6f33
jie <906038944@qq.com>
2013-08-27 11:17:11 +0800

    daily change

--- transmition.h
+++ transmition.h
@@ -3,14 +3,6 @@
 
 #define NAME_LEN 128
 
-enum message_type {
-	MSG_FILE_REQ,
-	MSG_FILE_CFM,
-	MSG_FILE_START,		/* file data is this message's data */
-	MSG_ONLINE,
-	MSG_OFFLINE,
-	MSG_CHAT,
-};
 struct host_raw {
 	char name[NAME_LEN];
 };
@@ -22,23 +14,4 @@ private:
 	string name;
 	string ip;	
 };
-
-struct msg_raw {
-	int msg_type;
-	int data_size;
-}
-
-	
-class message {
-public:
-	int send();
-	int receive();
-	message(int mtype);
-	
-	struct msg_raw *netpack;
-	int msg_type;
-	void *msg_data;
-private:
-};
-
 #endif
